356638a7aff3f10e49867724d5e5eda39eaf14a4
William Xavier <william.santos@aluno.unb.br>
2024-11-28 23:53:12 -0300

    exo-inicial

---
+++ Contexts/ExoContext.cs
@@ -0,0 +1,25 @@
+using Exo.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+
+namespace Exo.WebApi.Contexts
+{
+    public class ExoContext : DbContext
+    {
+        public ExoContext()
+        {
+        }
+        public ExoContext(DbContextOptions<ExoContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //essa string de conexao depente da sua maquina.
+                optionsBuilder.UseSqlServer("Server=HP\\SQLEXPRESS;" + "Database=ExoApi;Trusted_Connection=True;");
+            }
+        }
+        public DbSet<Projeto> Projetos { get; set; }// será criado um model projeto
+    }
+}
